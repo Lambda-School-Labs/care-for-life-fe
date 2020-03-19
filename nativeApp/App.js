a4f21938a0c70b106c75d162b33584ec38496887
Alexis <alexisdavalos.tech@gmail.com>
2020-03-19 17:41:29 -0400

    updated App Title

--- nativeApp/App.js
+++ nativeApp/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Care For Life</Text>
+      <Text>Care For Life - Native App</Text>
     </View>
   );
 }
