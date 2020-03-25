c325e7ad54cb2a27644e1dcc8c99d2db81432996
Alexis <alexisdavalos.tech@gmail.com>
2020-03-25 17:16:23 -0400

    Survey Progress Bar

--- nativeApp/screens/SurveyCompletedScreen.js
+++ nativeApp/screens/SurveyCompletedScreen.js
@@ -1,33 +1,68 @@
-import React from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
-import { Bar }  from 'react-native-progress';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
+import { Bar } from "react-native-progress";
+import ProgressBar from "@kcodev/react-native-progress-bar";
 
-const SurveyCompletedScreen = ({navigation}) => {
-  const progressNumber = 1.0;
+const SurveyCompletedScreen = ({ navigation }) => {
+  const [progress, setProgress] = useState(0.55);
+
+  useEffect(() => {}, [progress]);
+
   return (
     <View style={styles.container}>
       <Text>Survey completed</Text>
-      <Bar style={styles.bar} progress={progressNumber} color={'#333'} borderWidth={2} borderColor={'black'} />
-      <Text>Progress: {progressNumber * 100} %</Text>
-      <Button
-        title="Go home"
-        onPress={() => navigation.navigate('SurveyHome')}
+      <Bar
+        style={styles.bar}
+        progress={progress}
+        width={400}
+        color={"#333"}
+        borderWidth={2}
+        borderColor={"black"}
+        useNativeDriver={true}
       />
+      <ProgressBar
+        value={70}
+        maxValue={100}
+        backgroundColorOnComplete="#333"
+        backgroundColor="#333"
+      />
+      {/* Buttons */}
+      <Text>Progress: {progress * 100} %</Text>
+      <View style={styles.button}>
+        <Button title="Set To 100%" onPress={() => setProgress(1.0)} />
+      </View>
+      <View style={styles.button}>
+        <Button title="Set To 75%" onPress={() => setProgress(0.75)} />
+      </View>
+      <View style={styles.button}>
+        <Button title="Set To 50%" onPress={() => setProgress(0.5)} />
+      </View>
+      <View style={styles.button}>
+        <Button title="Set To 25%" onPress={() => setProgress(0.25)} />
+      </View>
+      <View style={styles.button}>
+        <Button
+          title="Go home"
+          onPress={() => navigation.navigate("SurveyHome")}
+        />
+      </View>
     </View>
-  )
-}
-
+  );
+};
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    alignItems: 'center',
-    justifyContent: 'center'
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
   },
-  bar:{
+  bar: {
     margin: 20,
-    width: '80%'
+  },
+  button: {
+    width: "80%",
+    margin: 10
   }
-})
+});
 
 export default SurveyCompletedScreen;
--- nativeApp/screens/SurveyScreen.js
+++ nativeApp/screens/SurveyScreen.js
@@ -1,32 +1,29 @@
-import React from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
-import { Bar }  from 'react-native-progress';
+import React from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
 
-const SurveyScreen = ({navigation}) => {
+const SurveyScreen = ({ navigation }) => {
   const progressNumber = 0.0;
   return (
     <View style={styles.container}>
       <Text>Survey goes here</Text>
-      <Bar style={styles.bar} progress={progressNumber} width={400} color={'#333'} borderWidth={2} borderColor={'black'} />
-      <Text>Progress: {progressNumber * 100} %</Text>
       <Button
         title="Complete the survey"
-        onPress={() => navigation.navigate('SurveyCompleted')}
+        onPress={() => navigation.navigate("SurveyCompleted")}
       />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    alignItems: 'center',
-    justifyContent: 'center'
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
   },
-  bar:{
+  bar: {
     margin: 20,
-    width: '80%'
+    width: "80%"
   }
-})
+});
 
 export default SurveyScreen;
