de070d67a624329580519ce81840bf8e796ea023
CAM603 <cameronhawley@comcast.net>
2020-04-13 16:46:31 -0400

    Update layout

--- nativeApp/screens/FamilyScreen.js
+++ nativeApp/screens/FamilyScreen.js
@@ -1,20 +1,26 @@
-import React, {useState} from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
-import { annualSurvey } from '../surveys/annualSurvey.js';
+import React, { useState } from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
+import { annualSurvey } from "../surveys/annualSurvey.js";
 
-const FamilyScreen = ({route, navigation}) => {
+const FamilyScreen = ({ route, navigation }) => {
   // Displays the family name
   // Ability to take surveys here
-  
+
   return (
     <View style={styles.container}>
       <View style={styles.buttonContainer}>
         <Button
           color="black"
           title="Annual Survey"
-          onPress={() => navigation.navigate('Survey', {familyName: route.params.familyName, survey: annualSurvey, type: 'Family'})}
+          onPress={() =>
+            navigation.navigate("Survey", {
+              familyName: route.params.familyName,
+              survey: annualSurvey,
+              type: "Family",
+            })
+          }
         />
-        <Button
+        {/* <Button
           color="black"
           title="Another Survey"
           onPress={() => console.log('Nothing here yet')}
@@ -23,30 +29,34 @@ const FamilyScreen = ({route, navigation}) => {
           color="black"
           title="Another Survey"
           onPress={() => console.log('Nothing here yet')}
-        />
+        /> */}
       </View>
-      <View style={{flex: 1, width: '80%', justifyContent: 'center'}}>
+      <View style={{ flex: 1, width: "80%", justifyContent: "center" }}>
         <Button
           color="green"
           title="Individual Survey"
-          onPress={() => navigation.navigate('FamilyMembers', {familyName: route.params.familyName})}
+          onPress={() =>
+            navigation.navigate("FamilyMembers", {
+              familyName: route.params.familyName,
+            })
+          }
         />
       </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   buttonContainer: {
-    width: '80%',
-    justifyContent: 'space-around',
-    flex: 1
-  }
-})
+    width: "80%",
+    justifyContent: "space-around",
+    flex: 1,
+  },
+});
 
 export default FamilyScreen;
