75476e2a29e1ebbe66a345f4755bf1caf5349379
KJ Magill <39092621+kjmagill@users.noreply.github.com>
2020-04-15 18:42:49 -0400

    Migrated to offix apolloOfflineClient

--- nativeApp/components/UploadData/index.js
+++ nativeApp/components/UploadData/index.js
@@ -1,80 +1,48 @@
 import React from 'react';
 import { gql, graphql } from 'react-apollo';
 
-const UploadData = ({ mutate }) => {
+const UploadDataBtn = ({ mutate }) => {
   return (
-    <button onClick={() => console.log('Hello world')}>Upload Data</button>
+    <button
+      onClick={() =>
+        console.log('Update this onClick to send mutation to gql endpoint')
+      }
+    >
+      Upload Data
+    </button>
   );
 };
 
 const uploadDataMutation = gql`
-  mutation ($familyName: String!){
-    createFamily(data:{
-      family_name: $familyName
-    }){
+  mutation($familyName: String!) {
+    createFamily(data: { family_name: $familyName }) {
       id
       family_name
     }
   }
-  mutation(
-    $personName: String!
-    $familyId: ID!
-  ){
-    createPerson(data: {
-      person_name: $personName
-      family: {
-        connect:{
-          id: $familyId
-        }
-      }
-    }){
+  mutation($personName: String!, $familyId: ID!) {
+    createPerson(
+      data: { person_name: $personName, family: { connect: { id: $familyId } } }
+    ) {
       person_name
       id
-      family{
+      family {
         id
         family_name
       }
     }
   }
-  mutation(
-    $surveyName: String!
-    $employeeId: ID!
-    $familyId: ID!
-  ){
-    createSurvey(data: {
-      survey_name: $surveyName
-      employee: {connect: {
-        id: $employeeId
-      }}
-      family: {connect:{
-        id: $familyId
-      }}
-    }){
+  mutation($surveyName: String!, $employeeId: ID!, $familyId: ID!) {
+    createSurvey(
+      data: {
+        survey_name: $surveyName
+        employee: { connect: { id: $employeeId } }
+        family: { connect: { id: $familyId } }
+      }
+    ) {
       id
       survey_name
     }
-  }
-  
-  // mutation($name: String!) {
-  //   family1: createFamily(data:{
-  //     family_name:$name
-  //   }) 
-  //   {
-  //     id
-  //   }
-  //   family2: createFamily(data:{
-  //     family_name:"another"
-  //   }) 
-  //   {
-  //     id
-  //   }
-  //   family3: createFamily(data:{
-  //     family_name:"another3"
-  //   }) 
-  //   {
-  //     id
-  //   }
-  // }
   }
 `;
 
