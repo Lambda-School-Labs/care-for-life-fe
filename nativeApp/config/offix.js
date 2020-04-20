d7fd2fc1dacb077fed5b898d9ff2bd220d8b0be1
KJ Magill <39092621+kjmagill@users.noreply.github.com>
2020-04-20 15:37:12 -0400

    Added createAnswers mutation

--- nativeApp/config/offix.js
+++ nativeApp/config/offix.js
@@ -1,14 +1,14 @@
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { HttpLink } from "apollo-link-http";
-import { AsyncStorage } from "react-native";
-import { ApolloOfflineClient } from "offix-client";
-import { NetworkStatus } from "./NetworkStatus";
-import "cross-fetch/polyfill";
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { AsyncStorage } from 'react-native';
+import { ApolloOfflineClient } from 'offix-client';
+import { NetworkStatus } from './NetworkStatus';
+import 'cross-fetch/polyfill';
 
 const cacheStorage = {
   getItem: async (key) => {
     const data = await AsyncStorage.getItem(key);
-    if (typeof data === "string") {
+    if (typeof data === 'string') {
       // console.log('Get item string', data);
       return JSON.parse(data);
     }
@@ -17,7 +17,7 @@ const cacheStorage = {
   },
   setItem: (key, value) => {
     let valueStr = value;
-    if (typeof valueStr === "object") {
+    if (typeof valueStr === 'object') {
       valueStr = JSON.stringify(value);
     }
     // console.log('Set item', valueStr);
@@ -30,22 +30,17 @@ const cacheStorage = {
 
 const networkStatus = new NetworkStatus();
 
-const token = process.env.APOLLO_TOKEN;
+// const token = process.env.APOLLO_TOKEN;
+const token =
+  'eyJraWQiOiI0dFJQSzU1STVkaW9TYi1la2xEQW9Ba0Zpb0piNWlCSjJsU3k0Z0J0OHpvIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULnUzQkxfWFlnWWUxYXZMNndCOFpFUDRGWWZJaGNIa2hGcXJKYUp0QXlVRFkiLCJpc3MiOiJodHRwczovL29rdGEuY2FyZWZvcmxpZmUuZGV2L29hdXRoMi9kZWZhdWx0IiwiYXVkIjoiYXBpOi8vZGVmYXVsdCIsImlhdCI6MTU4NzM5OTE0OSwiZXhwIjoxNTg3NDAyNzQ5LCJjaWQiOiIwb2E1aXhoaDJKODl3V0F6ZDR4NiIsInVpZCI6IjAwdTVpdzR2YXo2UldTbjk5NHg2Iiwic2NwIjpbIm9wZW5pZCJdLCJzdWIiOiJ0ZXN0QGV4YW1wbGUuY29tIn0.PE3r35eucsuk9E5WoPCVdY-5yxqJqBIqf9nBadOD23tg6YeEhfhKTIFTdyF8lf9dzrWGIRDiwaYmjU34lFIl20GVJGKDDvwiBw1uYBW7C97b5KjnvdvF-Ont5gO4wdSBUh5Rl4O4fm3CofZ9OR0zzDScHdxMITg0rrG7jUIMgIucBVF6p-Sbk_J3IiZ_OPVMzAp-xBvnPmt9mlWVK1-UhfTqO7bwzsO1wPeoYcgWqK8xBbpZE44-JunZy8EM5em41MGIY7a7W89r5lIYRu3kye_RjlGFNDhF0Gx28Gf5G5-PfUWwOgqRi9lxC-Zul_P3XRtqhq2tWTXgGbeDqVEa5Q';
 
 export const offlineClient = new ApolloOfflineClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: "https://apollo.careforlife.dev",
+    uri: 'https://apollo.careforlife.dev',
     headers: {
       authorization: `Bearer ${token}`,
     },
-    // request: (operation) => {
-    //   operation.setContext({
-    //     headers: {
-    //       authorization: `Bearer ${token}`,
-    //     },
-    //   });
-    // },
   }),
   offlineStorage: cacheStorage,
   cacheStorage,
--- nativeApp/screens/SurveyCompletedScreen.js
+++ nativeApp/screens/SurveyCompletedScreen.js
@@ -1,38 +1,45 @@
-import React, { useState, useEffect } from "react";
-import { ScrollView, StyleSheet, Text, View, Button } from "react-native";
-import { useOfflineMutation } from "react-offix-hooks";
-import { addFamilyAnswers } from "../Queries/queries";
+import React, { useState, useEffect } from 'react';
+import { ScrollView, StyleSheet, Text, View, Button } from 'react-native';
+import { useOfflineMutation } from 'react-offix-hooks';
+import { addAnswersMutation } from '../Queries/queries';
 
 const SurveyCompletedScreen = (props) => {
-  const [addAnswers, state] = useOfflineMutation(addFamilyAnswers);
+  const [addAnswers, state] = useOfflineMutation(addAnswersMutation);
 
   const answers = props.route.params.surveyAnswers;
   const familyName = props.route.params.familyName;
   const personName = props.route.params.personName;
   const type = props.route.params.type;
 
-  // console.log("Survey Answers:", answers);
+  const handleSubmit = (props) => {
+    props.navigation.navigate('Family', {
+      survey: answers,
+      familyName: familyName,
+    });
+    answers.map((answer) => {
+      addAnswers({
+        variables: {
+          answer: answer.value.value ? answer.value.value : answer.value,
+          questionId: answer.questionId,
+          familyId: 'ck906465v3s860874bvimqpc9',
+          surveyId: 'ck98pnlc17hmd0874rfdsxug0',
+        },
+      });
+    });
+  };
+
   let button;
   let text;
-  if (type === "Family") {
-    button = (
-      <Button
-        title="Go Home"
-        onPress={() =>
-          props.navigation.navigate("Family", {
-            survey: answers,
-            familyName: familyName,
-          })
-        }
-      />
-    );
+
+  if (type === 'Family') {
+    button = <Button title="Go Home" onPress={handleSubmit} />;
     text = <Text>{familyName} Family</Text>;
-  } else if (type === "Person") {
+  } else if (type === 'Person') {
     button = (
       <Button
         title="Go Home"
         onPress={() =>
-          props.navigation.navigate("FamilyMembers", {
+          props.navigation.navigate('FamilyMembers', {
             survey: answers,
             familyName: familyName,
             personName: personName,
@@ -50,7 +57,7 @@ const SurveyCompletedScreen = (props) => {
           {answers.map((el) => (
             // Here, I just used JavaScript to display the questionId and answer value
             <Text key={Math.random()}>
-              {el.questionId.replace(/_/g, " ")}:{" "}
+              {el.questionId.replace(/_/g, ' ')}:{' '}
               {el.value.value ? el.value.value : el.value}
             </Text>
           ))}
@@ -65,19 +72,19 @@ const SurveyCompletedScreen = (props) => {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "white",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
   },
   container: {
-    minWidth: "70%",
-    maxWidth: "90%",
-    alignItems: "stretch",
-    justifyContent: "center",
-    backgroundColor: "white",
+    minWidth: '70%',
+    maxWidth: '90%',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    backgroundColor: 'white',
     elevation: 20,
     borderRadius: 10,
-    maxHeight: "80%",
+    maxHeight: '80%',
   },
   questionText: {
     marginBottom: 20,
