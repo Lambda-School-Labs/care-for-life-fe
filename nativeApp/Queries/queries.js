64d3ab1e6882bb78d8328a098920c56b10051583
KJ Magill <39092621+kjmagill@users.noreply.github.com>
2020-04-23 15:21:20 -0400

    Updated offline mutations

--- nativeApp/Queries/queries.js
+++ nativeApp/Queries/queries.js
@@ -1,28 +1,127 @@
 import gql from 'graphql-tag';
 
-const addFamilyMutation = gql`
-  mutation($familyName: String!) {
-    createFamily(data: { family_name: $familyName }) {
-      id
-      family_name
-    }
-  }
-`;
-const addAnswersMutation = gql`
-  mutation($answer: String!, $questionId: ID!, $familyId: ID!, $surveyId: ID!) {
-    createAnswer(
+// creates a family and all of their survey answers in the database
+const addFamilyAndAnswersMutation = gql`
+  mutation(
+    $familyName: String!
+    $surveyName: String!
+    $employeeId: ID!
+    $answerText: String!
+    $questionId: ID!
+  ) {
+    createFamily(
       data: {
-        answer: $answer
-        question: { connect: { id: $questionId } }
-        family: { connect: { id: $familyId } }
-        survey: { connect: { id: $surveyId } }
+        family_name: $familyName
+        surveys: {
+          create: {
+            survey_name: $surveyName
+            employee: { connect: { id: $employeeId } }
+            answers: {
+              create: {
+                answer: $answerText
+                question: { connect: { id: $questionId } }
+              }
+            }
+          }
+        }
       }
     ) {
-      answer
-      dateTaken
       id
+      family_name
+      family_members
     }
   }
 `;
 
-export { addFamilyMutation, addAnswersMutation };
+// creates an individual family member and all of their solo survey answers in the database
+const addIndividualAndAnswersMutation = gql`
+  mutation(
+    $personName: String!
+    $surveyName: String!
+    $employeeId: ID!
+    $answerText: String!
+    $questionId: ID!
+  ) {
+    createPerson(
+      data: {
+        person_name: $personName
+        surveys: {
+          create: {
+            survey_name: $surveyName
+            employee: { connect: { id: $employeeId } }
+            answers: {
+              create: {
+                answer: $answerText
+                question: { connect: { id: $questionId } }
+              }
+            }
+          }
+        }
+      }
+    ) {
+      id
+      person_name
+    }
+  }
+`;
+
+// adds a new user (aka a CFL employee) to the database, then returns a JWT token & the user's info
+const signUpMutation = gql`
+  mutation signUp($name: String!, $email: String!, $password: String!) {
+    signUp(name: $name, email: $email, password: $password) {
+      token
+      user {
+        id
+        name
+        email
+      }
+    }
+  }
+`;
+
+// checks a user's credentials upon logging in, then returns a JWT token & the user's info
+const loginMutation = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        id
+        name
+        email
+      }
+    }
+  }
+`;
+
+// const addFamilyMutation = gql`
+//   mutation($familyName: String!) {
+//     createFamily(data: { family_name: $familyName }) {
+//       id
+//       family_name
+//     }
+//   }
+// `;
+
+// const addAnswersMutation = gql`
+//   mutation($answer: String!, $questionId: ID!, $familyId: ID!, $surveyId: ID!) {
+//     createAnswer(
+//       data: {
+//         answer: $answer
+//         question: { connect: { id: $questionId } }
+//         family: { connect: { id: $familyId } }
+//         survey: { connect: { id: $surveyId } }
+//       }
+//     ) {
+//       answer
+//       dateTaken
+//       id
+//     }
+//   }
+// `;
+
+export {
+  addFamilyAndAnswersMutation,
+  addIndividualAndAnswersMutation,
+  signUpMutation,
+  loginMutation,
+};
--- nativeApp/screens/SurveyCompletedScreen.js
+++ nativeApp/screens/SurveyCompletedScreen.js
@@ -1,11 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { ScrollView, StyleSheet, Text, View, Button } from "react-native";
-import { useOfflineMutation } from "react-offix-hooks";
-import { addFamilyMutation } from "../Queries/queries";
+import React from 'react';
+import { ScrollView, StyleSheet, Text, View, Button } from 'react-native';
+import { useOfflineMutation } from 'react-offix-hooks';
+import { addFamilyAndAnswersMutation } from '../Queries/queries';
 
 const SurveyCompletedScreen = (props) => {
-  const [addFamily, state] = useOfflineMutation(addFamilyMutation);
+  const [addFamilyAndAnswers, state] = useOfflineMutation(
+    addFamilyAndAnswersMutation
+  );
 
+  // need to store the backend ID returned during login/registration and set it to equal the userId variable on the line below
+  const userId = 'Insert ID of the logged in user';
   const answers = props.route.params.surveyAnswers;
   const fullSurvey = props.route.params.fullSurvey;
   const familyName = props.route.params.familyName;
@@ -13,33 +17,34 @@ const SurveyCompletedScreen = (props) => {
   const type = props.route.params.type;
 
   const handleSubmit = async (props) => {
-    console.log("Submitting Answers....", fullSurvey);
+    console.log('Submitting Answers....', fullSurvey);
 
     await answers.forEach((answer, index) => {
-      console.log("answer being mutated", answer.value);
-      console.log("answers backendID", fullSurvey[index].backend_id);
+      console.log('answer being mutated', answer.value);
+      console.log('answers backendID', fullSurvey[index].backend_id);
       try {
-        addAnswers({
+        addFamilyAndAnswers({
           variables: {
-            answer: answer.value.value
+            familyName: familyName,
+            surveyName: 'Family Annual Survey',
+            employeeId: userId,
+            answerText: answer.value.value
               ? answer.value.value.toString()
               : answer.value.toString(),
             questionId: fullSurvey[index].backend_id,
-            familyId: "ck9a3uqh384530874p5ws4zo8",
-            surveyId: "ck98pnlc17hmd0874rfdsxug0",
           },
         });
       } catch (error) {
         if (error.offline) {
           error
             .watchOfflineChange()
-            .then((res) => console.log("Offline result", res));
+            .then((res) => console.log('Offline result', res));
         }
         console.log(error);
       }
     });
 
-    props.navigation.navigate("Family", {
+    props.navigation.navigate('Family', {
       survey: answers,
       familyName: familyName,
     });
@@ -48,15 +53,15 @@ const SurveyCompletedScreen = (props) => {
   let button;
   let text;
 
-  if (type === "Family") {
+  if (type === 'Family') {
     button = <Button title="Go Home" onPress={() => handleSubmit(props)} />;
     text = <Text>{familyName} Family</Text>;
-  } else if (type === "Person") {
+  } else if (type === 'Person') {
     button = (
       <Button
         title="Go Home"
         onPress={() =>
-          props.navigation.navigate("FamilyMembers", {
+          props.navigation.navigate('FamilyMembers', {
             survey: answers,
             familyName: familyName,
             personName: personName,
@@ -74,7 +79,7 @@ const SurveyCompletedScreen = (props) => {
           {answers.map((el) => (
             // Here, I just used JavaScript to display the questionId and answer value
             <Text key={Math.random()}>
-              {el.questionId.replace(/_/g, " ")}:{" "}
+              {el.questionId.replace(/_/g, ' ')}:{' '}
               {el.value.value ? el.value.value : el.value}
             </Text>
           ))}
@@ -89,19 +94,19 @@ const SurveyCompletedScreen = (props) => {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "white",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
   },
   container: {
-    minWidth: "70%",
-    maxWidth: "90%",
-    alignItems: "stretch",
-    justifyContent: "center",
-    backgroundColor: "white",
+    minWidth: '70%',
+    maxWidth: '90%',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    backgroundColor: 'white',
     elevation: 20,
     borderRadius: 10,
-    maxHeight: "80%",
+    maxHeight: '80%',
   },
   questionText: {
     marginBottom: 20,
