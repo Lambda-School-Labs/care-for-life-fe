8b1660dd98e947af5ec0e28bc68d41ba11a5cfea
Alexis <alexisdavalos.tech@gmail.com>
2020-04-23 19:42:32 -0400

    Okta Setup

---
+++ nativeApp/api/oktaConfig.js
@@ -0,0 +1,14 @@
+import {
+  CLIENT_ID,
+  ISSUER,
+  DEV_URI,
+  PRODUCTION_URI,
+} from "react-native-dotenv";
+export default {
+  clientId: { CLIENT_ID },
+  // For usage in bare and standalone
+  redirectUri: { PRODUCTION_URI },
+  // For usage in managed apps using the proxy
+  redirectUri: { DEV_URI },
+  scopes: ["openid", "profile"],
+};
