63a93c92984e4311553bfb48505cbb7be600d4c4
Brandon <teaguebrandon@outlook.com>
2020-06-01 16:06:10 -0400

    Created redux folders and redux store

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import React from "react";
 import Routes from "./src/routes";
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+let store = createStore(combineReducers({}));
 
 export default function App() {
-  return <Routes />;
+  return (
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  )
 }
