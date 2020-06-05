52d838b1722fdf73ab2d5331829fe0e9ced43058
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-06-05 13:31:12 -0700

    create auth router

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { View, Text, Button } from "react-native";
 //import { createConfig, isAuthenticated, getUser, getUserFromIdToken } from "@okta/okta-react-native";
 import config from '../okta/index';
@@ -8,8 +8,6 @@ import axios from 'axios';
 
 export default function HomeScreen({ route, navigation }) {
 
-  //const [authenticated, setAuthenticated] = useState(false)
-
   const { idToken } = route.params;
   const { accessToken } = route.params;
 
@@ -17,14 +15,7 @@ export default function HomeScreen({ route, navigation }) {
   const getUserInfo = () => {
     console.log('id token', idToken)
     console.log('access token', accessToken)
-    // axios.post(`https://dev-815303.okta.com/oauth2/default/v1/introspect?client_id=${CLIENT_ID}&token=${idToken}&token_type_hint=id_token`, {
-    //   headers: {
-    //     Authorization: accessToken,
-    //     'Content-type': 'application/x-www-form-urlencoded'
-    //   }
-    // })
-    // axios.get('https://care-for-life.herokuapp.com/api/zones')
-    axios.get(`http://${IP}:5600/login`, {
+    axios.get(`http://${IP}:5600/auth/login`, {
       headers: {
         Authorization: `${idToken}`
       }
@@ -34,28 +25,8 @@ export default function HomeScreen({ route, navigation }) {
   }
 
   useEffect(() => {
-    //console.log("***id token ***", idToken)
-    // createConfig({
-    //   clientId: CLIENT_ID,
-    //   redirectUri: config.redirectUri,
-    //   endSessionRedirectUri: REDIRECT_URI,
-    //   discoveryUri: ISSUER,
-    //   scopes: config.scopes,
-    //   requireHardwareBackedKeyStore: false,
-    // });
-    // checkAuthentication();
     getUserInfo();
-
   }, [])
-
-  // const checkAuthentication = async () => {
-  //   const result = await isAuthenticated()
-  //     .then(res => console.log(res))
-  //     .catch(err => console.log(err));
-  //   if (result.authenticated !== authenticated) {
-  //     setAuthenticated(!authenticated)
-  //   }
-  // }
 
   return (
     <View style={styles.screen}>
