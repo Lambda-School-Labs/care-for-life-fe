5c112fecaafdc1643d614adaa2572500ef0c979a
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-06-15 15:03:26 -0700

    update userReducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -6,7 +6,8 @@ let initialState = {
   email: '',
   zone_id: 0,
   community_id: 0,
-  user_id: 0
+  user_id: 0,
+  role_name: ''
 };
 
 export default (state = initialState, action) => {
@@ -23,7 +24,8 @@ export default (state = initialState, action) => {
         email: action.payload.email,
         zone_id: action.payload.zone_id,
         community_id: action.payload.community_id,
-        user_id: action.payload.id
+        user_id: action.payload.id,
+        role_name: action.payload.role_name
       };
     default:
       return state;
